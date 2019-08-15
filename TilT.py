872cf1bb71438b6ad46655b98c25441f0c014f39
kaitoyamagishi1109 <kaitoyamagishi1109@gmail.com>
2019-08-15 14:38:17 +0900

    made code modular w/ functions

--- TilT.py
+++ TilT.py
@@ -4,15 +4,9 @@ from datetime import datetime
 from pytz import timezone
 
 #variable initializations
-#count of trains registered as an object for each direction
-toBC = 0
-toPS = 0
 timenow = datetime.now(timezone("US/Eastern"))
 #list of vehicle objects
 myVehicles = []
-index = 0
-vehicleslist = ""
-
 #dict containing all stop ids for stop names
 stops = {
     #direction_id = 0, headed to boston college
@@ -69,62 +63,76 @@ stops = {
 
 #class initialization
 class Vehicles:
-    def __init__(self, direction_id, timetil, vehicle_id):
+    def __init__(self, direction_id, timetil, vehicle_id, current_status = "", stop = ""):
         self.direction_id = direction_id
         self.timetil = timetil
         self.vehicle_id = vehicle_id
         #under this comment are attributes added to the object from the oher API response
-        # self.current_status = current_status
-        # self.stop = stop
+        self.current_status = current_status
+        self.stop = stop
 
-#API call
-predictions = requests.get("https://api-v3.mbta.com/predictions?sort=arrival_time&filter%5Bstop%5D=70113%2C70112").json()
+#Find vehicles, 
+def findVehicles():
+    #count of trains registered as an object for each direction
+    toBC = 0
+    toPS = 0
+    #API call
+    predictions = requests.get("https://api-v3.mbta.com/predictions?sort=arrival_time&filter%5Bstop%5D=70113%2C70112").json()
+    #Iterate through response from predictions API until 2 vehicles for each direction is recieved
+    while (len(myVehicles) < 4):
+        for i in (predictions["data"]):
+            #if direction is 0 (to BC), vehicle data is present and there is less than 2 vehicle data for this direction
+            if ((i["attributes"]["direction_id"] == 0) and (i["relationships"]["vehicle"]["data"] != None) and (toBC < 2)):
+                #block of code to get the time until the next vehicle
+                timetil0 = i["attributes"]["arrival_time"]
+                datetime0 = datetime.strptime(timetil0, "%Y-%m-%dT%H:%M:%S%z")
+                mintilarrival0 = round((abs(datetime0-timenow).total_seconds())/60)
+                #Create a new object with the direction id, minutes until arrival and the vehicle id
+                myVehicles.append(Vehicles(0,mintilarrival0,i["relationships"]["vehicle"]["data"]["id"]))
+                toBC += 1
+            #if direction is 1 (to park street), vehicle data is present and there is less than 2 vehicle data for this direction
+            elif ((i["attributes"]["direction_id"] == 1) and (i["relationships"]["vehicle"]["data"] != None) and (toPS < 2)):
+                #block of code to get the time until the next vehicle
+                timetil1 = i["attributes"]["arrival_time"]
+                datetime1 = datetime.strptime(timetil1, "%Y-%m-%dT%H:%M:%S%z")
+                mintilarrival1 = round((datetime1-timenow).total_seconds()/60)
+                #Create a new object with the direction id, minutes until arrival and the vehicle id
+                myVehicles.append(Vehicles(1,mintilarrival1,i["relationships"]["vehicle"]["data"]["id"]))
+                toPS += 1
+            #safety catch (usually gets trapped here since train is missing vehicle ID)
+            else:
+                break
+        #if the whole API response gets iterated but doesn't reach 4 vehicles, break
+        break
 
-#Iterate through response from predictions API until 2 vehicles for each direction is recieved
-while (len(myVehicles) < 4):
-    for i in (predictions["data"]):
-        #if direction is 0 (to BC), vehicle data is present and there is less than 2 vehicle data for this direction
-        if ((i["attributes"]["direction_id"] == 0) and (i["relationships"]["vehicle"]["data"] != None) and (toBC < 2)):
-            #block of code to get the time until the next vehicle
-            timetil0 = i["attributes"]["arrival_time"]
-            datetime0 = datetime.strptime(timetil0, "%Y-%m-%dT%H:%M:%S%z")
-            mintilarrival0 = round((abs(datetime0-timenow).total_seconds())/60)
-            #Create a new object with the direction id, minutes until arrival and the vehicle id
-            myVehicles.append(Vehicles(0,mintilarrival0,i["relationships"]["vehicle"]["data"]["id"]))
-            toBC += 1
-        #if direction is 1 (to park street), vehicle data is present and there is less than 2 vehicle data for this direction
-        elif ((i["attributes"]["direction_id"] == 1) and (i["relationships"]["vehicle"]["data"] != None) and (toPS < 2)):
-            #block of code to get the time until the next vehicle
-            timetil1 = i["attributes"]["arrival_time"]
-            datetime1 = datetime.strptime(timetil1, "%Y-%m-%dT%H:%M:%S%z")
-            mintilarrival1 = round((datetime1-timenow).total_seconds()/60)
-            #Create a new object with the direction id, minutes until arrival and the vehicle id
-            myVehicles.append(Vehicles(1,mintilarrival1,i["relationships"]["vehicle"]["data"]["id"]))
-            toPS += 1
-        #safety catch (usually gets trapped here since train is missing vehicle ID)
-        else:
-            break
-    #if the whole API response gets iterated but doesn't reach 4 vehicles, break
-    break
+def findStatus():
+    #string of vehicle ids to pass to api
+    vehicleslist = ""
+    index = 0
+    #Iterate through the vehicle object list and concatinate vehicle name, now ready to pass to the API
+    for i in myVehicles:
+        vehicleslist += (i.vehicle_id+"%2C")
+    vehicleslist = vehicleslist[:-3]
 
-#Iterate through the vehicle object list and concatinate vehicle name, now ready to pass to the API
-for j in myVehicles:
-    vehicleslist += (j.vehicle_id+"%2C")
-vehicleslist = vehicleslist[:-3]
+    #call vehicles API to fetch vehicle data
+    vehiclesdata = requests.get("https://api-v3.mbta.com/vehicles?filter%5Bid%5D=" + vehicleslist).json()
+    for j in (vehiclesdata["data"]):
+        #register attributes current_status and stop
+        myVehicles[index].current_status = j["attributes"]["current_status"]
+        stopid = j["relationships"]["stop"]["data"]["id"]
+        myVehicles[index].stop = stops[stopid]
+        index += 1
 
-#call vehicles API to fetch vehicle data
-vehiclesdata = requests.get("https://api-v3.mbta.com/vehicles?filter%5Bid%5D=" + vehicleslist).json()
-for m in (vehiclesdata["data"]):
-    #register attributes current_status and stop
-    myVehicles[index].current_status = m["attributes"]["current_status"]
-    stopid = m["relationships"]["stop"]["data"]["id"]
-    myVehicles[index].stop = stops[stopid]
-    index += 1
+def main():
+    #print to check
+    for i in myVehicles:
+        print("Direction is " + str(i.direction_id))
+        print("Arriving in " + str(i.timetil) + " minute(s)")
+        print("ID is " + i.vehicle_id)
+        print("Vehicle is " + i.current_status)
+        print(i.stop)
 
-#print to check
-for p in myVehicles:
-    print("Direction is " + str(p.direction_id))
-    print("Arriving in " + str(p.timetil) + " minute(s)")
-    print("ID is " + p.vehicle_id)
-    print("Vehicle is " + p.current_status)
-    print(p.stop)
+if __name__ == '__main__':
+    findVehicles()
+    findStatus()
+    main()
