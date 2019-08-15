33233db6d0d8e1b1927bf372bec7205a7889aba8
kaitoyamagishi1109 <kaitoyamagishi1109@gmail.com>
2019-08-15 10:25:46 +0900

    working version

--- TilT.py
+++ TilT.py
@@ -3,31 +3,89 @@ import requests
 from datetime import datetime
 from pytz import timezone
 
-predictions = requests.get("https://api-v3.mbta.com/predictions?sort=arrival_time&filter%5Bstop%5D=70145%2C70144").json()
-
+#variable initializations
 toBC = 0
 toPS = 0
-
 timenow = datetime.now(timezone("US/Eastern"))
-
 vehicles = []
+stops = {
+    #direction_id = 0, headed to boston college
+    70107 : "Boston College",
+    70111 : "South Street",
+    70113 : "Chestnut Hill Avenue",
+    70115 : "Chiswick Road",
+    70117 : "Sutherland Road",
+    70121 : "Washington Street",
+    70125 : "Warren Street",
+    70127 : "Allston Street",
+    70129 : "Griggs Street",
+    70131 : "Harvard Avenue",
+    70135 : "Packards Corner",
+    70137 : "Babcock Street",
+    70139 : "Pleasant Street",
+    70143 : "Boston University West",
+    70145 : "Boston University Central",
+    70147 : "Boston University East",
+    70149 : "Blandford Street",
+    71151 : "Kenmore",
+    70153 : "Hynes Convention Center",
+    70155 : "Copley",
+    70157 : "Arlington",
+    70159 : "Boylston",
+    70196 : "Park Street",
+    #direction_id = 1, headed to park street
+    70106 : "Boston College",
+    70110 : "South Street",
+    70112 : "Chestnut Hill Avenue",
+    70114 : "Chiswick Road",
+    70116 : "Sutherland Road",
+    70120 : "Washington Street",
+    70124 : "Warren Street",
+    70126 : "Allston Street",
+    70128 : "Griggs Street",
+    70130 : "Harvard Avenue",
+    70134 : "Packards Corner",
+    70136 : "Babcock Street",
+    70138 : "Pleasant Street",
+    70142 : "Boston University West",
+    70144 : "Boston University Central",
+    70146 : "Boston University East",
+    70148 : "Blandford Street",
+    70150 : "Kenmore",
+    70152 : "Hynes Convention Center",
+    70154 : "Copley",
+    70156 : "Arlington",
+    70158 : "Boylston",
+    70199 : "Park Street"
+}
 
-#Iterate through response from predictions API: Find two train IDs of closest trains and put them into vehicles list
+#API call
+predictions = requests.get("https://api-v3.mbta.com/predictions?sort=arrival_time&filter%5Bstop%5D=70145%2C70144").json()
+
+#Iterate through response from predictions API: Find two train IDs of closest trains and put them into list "vehicles"
 for i in (predictions["data"]):
     if (toBC < 1) or (toPS < 1):
         if (i["attributes"]["direction_id"] == 0):
             vehicles.append(i["relationships"]["vehicle"]["data"]["id"])
+            #block of code to get the time until the next vehicle
             timetil0 = i["attributes"]["arrival_time"]
             datetime0 = datetime.strptime(timetil0, "%Y-%m-%dT%H:%M:%S%z")
             mintilarrival0 = round((abs(datetime0-timenow).total_seconds())/60)
-            print("Next train to Boston College arriving in " + str(mintilarrival0))
+            print("Next train headed to Boston College arriving in " + str(mintilarrival0) + " minutes.")
             toBC += 1
         elif (i["attributes"]["direction_id"] == 1):
             vehicles.append(i["relationships"]["vehicle"]["data"]["id"])
+            #block of code to get the time until the next vehicle
             timetil1 = i["attributes"]["arrival_time"]
             datetime1 = datetime.strptime(timetil1, "%Y-%m-%dT%H:%M:%S%z")
             mintilarrival1 = round((abs(datetime1-timenow).total_seconds())/60)
-            print("Next train to Park Street arriving in " + str(mintilarrival1))
+            print("Next train to headed Park Street arriving in " + str(mintilarrival1) + " minutes.")
             toPS += 1
 
-print(vehicles)
+#concatinate the items in the list "vehicles" in a format specified by the API call
+vehicleslist = "%2C".join(vehicles)
+#call vehicles API to fetch vehicle data
+vehiclesdata = requests.get("https://api-v3.mbta.com/vehicles?sort=direction_id&filter%5Bid%5D=" + vehicleslist).json()
+for j in (vehiclesdata["data"]):
+    stopid = int(j["relationships"]["stop"]["data"]["id"])
+    print(j["attributes"]["current_status"] + " " + stops[stopid])
